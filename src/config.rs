c6d7e37226dbea7e40045e86a32105befe891eb7
VastSea0 <vastseaoffical0@outlook.com>
2025-08-22 23:32:26 +0300

    Update localization directory paths in config.rs for Notia application

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
 pub static VERSION: &str = "0.1.0";
 pub static GETTEXT_PACKAGE: &str = "notia";
-pub static LOCALEDIR: &str = "/usr/local/share/locale";
-pub static PKGDATADIR: &str = "/usr/local/share/notia";
+pub static LOCALEDIR: &str = "/home/helo/Projects/.gnome-builder/projects/Notia/install/host/share/locale";
+pub static PKGDATADIR: &str = "/home/helo/Projects/.gnome-builder/projects/Notia/install/host/share/notia";
