a305265bf9dbe42b0cd09533dc8ae80596a59a4b
VastSea0 <vastseaoffical0@outlook.com>
2025-08-22 22:41:29 +0300

    Add SidebarData struct and enhance sidebar methods
    Introduced SidebarData struct to encapsulate sidebar-related data. Added methods to update sidebar content, retrieve note text, clear note text, and connect save/clear note actions to callbacks. This enhances the sidebar's functionality and interactivity.

--- src/sidebar.rs
+++ src/sidebar.rs
@@ -2,6 +2,13 @@ use gtk::prelude::*;
 use gtk::subclass::prelude::*;
 use gtk::{glib, CompositeTemplate};
 
+pub struct SidebarData {
+    pub photo_path: Option<String>,
+    pub photo_name: Option<String>,
+    pub note_text: Option<String>,
+    pub note_status: Option<String>,
+}
+
 #[derive(Debug, CompositeTemplate)]
 #[template(resource = "/com/vastsea/notia/sidebar.ui")]
 pub struct Sidebar {
@@ -38,7 +45,6 @@ impl ObjectImpl for Sidebar {}
 impl WidgetImpl for Sidebar {}
 impl RevealerImpl for Sidebar {}
 
-// Public wrapper
 glib::wrapper! {
     pub struct Sidebar(ObjectSubclass<Sidebar>) @extends gtk::Widget, gtk::Revealer;
 }
@@ -46,5 +52,51 @@ glib::wrapper! {
 impl Sidebar {
     pub fn new() -> Self {
         glib::Object::builder().build()
+    }
+
+    pub fn update_sidebar(&self, data: SidebarData) {
+        let imp = self.imp();
+        if let Some(photo_name) = data.photo_name {
+            imp.photo_label.set_text(&photo_name);
+        }
+        if let Some(note_status) = data.note_status {
+            imp.note_label.set_text(&note_status);
+        }
+        if let Some(note_text) = data.note_text {
+            let buffer = imp.note_text_view.buffer();
+            buffer.set_text(&note_text);
+        }
+        // Fotoğraf önizlemesi için photo_path kullanılabilir
+        if let Some(photo_path) = data.photo_path {
+            let file = gio::File::for_path(photo_path);
+            imp.selected_photo_preview.set_file(Some(&file));
+        }
+    }
+
+    pub fn get_note_text(&self) -> String {
+        let imp = self.imp();
+        let buffer = imp.note_text_view.buffer();
+        let (start, end) = buffer.bounds();
+        buffer.text(&start, &end, false).to_string()
+    }
+
+    pub fn clear_note_text(&self) {
+        let imp = self.imp();
+        let buffer = imp.note_text_view.buffer();
+        buffer.set_text("");
+    }
+
+    pub fn connect_save_note<F: Fn() + 'static>(&self, callback: F) {
+        let imp = self.imp();
+        imp.save_note_button.connect_clicked(move |_| {
+            callback();
+        });
+    }
+
+    pub fn connect_clear_note<F: Fn() + 'static>(&self, callback: F) {
+        let imp = self.imp();
+        imp.clear_note_button.connect_clicked(move |_| {
+            callback();
+        });
     }
 }
